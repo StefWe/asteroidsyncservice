9e850f0b3abe44d2902d479c0d6a111722153e08
Stefan Weng <stefan.weng@gmx.net>
2020-07-30 20:24:16 +0200

    next try and error

--- prepare.sh
+++ prepare.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
- 
+cd .. 
 wget https://github.com/google/googletest/archive/release-1.8.1.tar.gz
 tar -xvzf release-1.8.1.tar.gz
-export GMOCK_HOME=~/gmock-1.8.1
-pwd
+export GMOCK_HOME=/home/travis/build/StefWe/gmock-1.8.1
 cd asteroidsyncservice/tests
 mkdir build 
 cd build 
