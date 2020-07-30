afb55cfe6d25cf828203b5503360d75c2694a60a
Stefan Weng <stefan.weng@gmx.net>
2020-07-30 20:17:18 +0200

    check path in ci

--- prepare.sh
+++ prepare.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-cd .. 
+ 
 wget https://github.com/google/googletest/archive/release-1.8.1.tar.gz
 tar -xvzf release-1.8.1.tar.gz
+export GMOCK_HOME=~/gmock-1.8.1
+pwd
 cd asteroidsyncservice/tests
 mkdir build 
 cd build 
